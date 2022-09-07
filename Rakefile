c39b1b3378c0d7d0497a6db84074f4b10bed4422
christian.koestlin <christian.koestlin@gmail.com>
2022-09-08 00:56:27 +0200

    feature: Create pages for the spec sections as well

--- Rakefile
+++ Rakefile
@@ -8,7 +8,7 @@ end
 SQLITE_DB = "D.docset/Contents/Resources/docSet.dsidx"
 desc "Gen sqlite index"
 task :gen do
-  ruby "gen.rb D.docset/Contents/Resources/Documents/phobos D.docset/Contents/Resources/Documents"
+  ruby "gen.rb D.docset/Contents/Resources/Documents D.docset/Contents/Resources/Documents phobos/Module spec/Section"
   rm SQLITE_DB if File.exist?(SQLITE_DB)
   sh "sqlite3 #{SQLITE_DB} < index.sql"
 end
--- gen.rb
+++ gen.rb
@@ -2,24 +2,33 @@ require "nokogiri"
 
 basedir = ARGV.shift
 reldir = ARGV.shift
+subpaths = ARGV
 
 out = File.open("index.sql","w")
 out.puts "CREATE TABLE IF NOT EXISTS searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
 out.puts "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);"
-Dir[File.join(basedir,"*")].each do |f|
-  puts "Working on #{f}"
-  doc = Nokogiri::HTML(IO.read(f))
-  title = doc.at_css(".hyphenate > h1")
-  next unless title
-  page_name = title.text
-  relpath = f[reldir.length+1..-1]
-  out.puts "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('#{page_name}', 'Module', '#{relpath}');"
-    doc.css(".d_decl > div > span").each do |el|
-    next unless el
-    anchor = el["id"]
-    next unless anchor
-    next unless anchor.start_with?(".")
-    name = anchor[1..-1]
-    out.puts "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('#{name}', 'Function', '#{relpath}##{anchor}');"
+subpaths.each do |subpathSpec|
+  subpath, type = subpathSpec.split("/")
+  Dir[File.join(basedir, subpath, "/*")].each do |f|
+    puts "Working on #{f}"
+    text = IO.read(f)
+    m = text.match(/counter-reset: h1 (\d+)/)
+    doc = Nokogiri::HTML(text)
+    title = doc.at_css(".hyphenate > h1")
+    next unless title
+    page_name = title.text
+    if m
+      page_name = "#{m[1].rjust(2, '0')} - #{page_name}"
+    end
+    relpath = f[reldir.length+1..-1]
+    out.puts "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('#{page_name}', '#{type}', '#{relpath}');"
+      doc.css(".d_decl > div > span").each do |el|
+      next unless el
+      anchor = el["id"]
+      next unless anchor
+      next unless anchor.start_with?(".")
+      name = anchor[1..-1]
+      out.puts "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('#{name}', 'Function', '#{relpath}##{anchor}');"
+    end
   end
 end
