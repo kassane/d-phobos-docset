228e52a396ec3d448c274d739355a47a00c2fd74
Christian Köstlin <christian.koestlin@gmail.com>
2022-10-01 02:13:19 +0200

    chore: Optimize ruby

--- gen.rb
+++ gen.rb
@@ -4,6 +4,7 @@ basedir = ARGV.shift
 reldir = ARGV.shift
 subpaths = ARGV
 
+counter_regexp = /counter-reset: h1 (\d+)/
 out = File.open("index.sql","w")
 out.puts "CREATE TABLE IF NOT EXISTS searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);"
 out.puts "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);"
@@ -12,7 +13,7 @@ subpaths.each do |subpathSpec|
   Dir[File.join(basedir, subpath, "/*")].each do |f|
     puts "Working on #{f}"
     text = IO.read(f)
-    m = text.match(/counter-reset: h1 (\d+)/)
+    m = text.match(counter_regexp)
     doc = Nokogiri::HTML(text)
     title = doc.at_css(".hyphenate > h1")
     next unless title
@@ -22,7 +23,7 @@ subpaths.each do |subpathSpec|
     end
     relpath = f[reldir.length+1..-1]
     out.puts "INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('#{page_name}', '#{type}', '#{relpath}');"
-      doc.css(".d_decl > div > span").each do |el|
+    doc.css(".d_decl > div > span").each do |el|
       next unless el
       anchor = el["id"]
       next unless anchor
